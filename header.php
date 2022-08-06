567ff5efff3bfc7a43b3c07ab2ee9e402e52de90
mourad <101376159+mourad-edine@users.noreply.github.com>
2022-08-06 08:50:07 +0300

    ajout d'un photo

--- header.php
+++ header.php
@@ -37,10 +37,9 @@
             <a class="nav-link " href="#">Disabled</a>
           </li>
         </ul>
-        <form class="d-flex">
-          <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-          <button class="btn btn-outline-success" type="submit">Search</button>
-        </form>
+        <div class="d-flex">
+              <img class="bd-placeholder-img rounded-circle" src="Fotoram.io.jpg" alt="" height="50">
+        </div>
       </div>
     </div>
   </nav>
